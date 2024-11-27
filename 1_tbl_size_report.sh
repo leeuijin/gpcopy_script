07964cb44163f7dcdda3ed57f6692640e31c0968
Uijin Lee <geartec82@gmail.com>
2024-11-27 16:42:37 +0900

    Update 1_tbl_size_report.sh

--- 1_tbl_size_report.sh
+++ 1_tbl_size_report.sh
@@ -7,7 +7,7 @@ psql -f ./sql/rpt_non_prt_tbl_all.sql > ./report/rpt_non_prt_tbl_all.$TODAY
 psql -f ./sql/rpt_prt_tbl_all.sql > ./report/rpt_prt_tbl_all.$TODAY
 
 echo '##############################################'
-echo '1.Please Check Report Files ...'
+echo '          1.Please Check Report Files ...     '
 echo '##############################################'
 
 ls -al ./report
